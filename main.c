19998a54c94b50b28791984f06d0f0f8e6f38cb0
Hans Naert <hans.naert@vives.be>
2024-02-21 08:16:30 +0100

    adapt for gtk4

--- main.c
+++ main.c
@@ -1,10 +1,28 @@
 #include <gtk/gtk.h>
 
-int main(int argc, char *argv[])
+static void
+activate (GtkApplication* app,
+          gpointer        user_data)
 {
-    gtk_init(&argc, &argv);
-    GtkWidget *win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_widget_show(win);
-    gtk_main();
-    return 0;
+  GtkWidget *window;
+
+  window = gtk_application_window_new (app);
+  gtk_window_set_title (GTK_WINDOW (window), "Window");
+  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+  gtk_window_present (GTK_WINDOW (window));
+}
+
+int
+main (int    argc,
+      char **argv)
+{
+  GtkApplication *app;
+  int status;
+
+  app = gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+  status = g_application_run (G_APPLICATION (app), argc, argv);
+  g_object_unref (app);
+
+  return status;
 }
